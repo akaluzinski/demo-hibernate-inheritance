f0f270bba2088e42893c9d1cf4844120876161b7
Adrian Kaluzinski <kaluza@gmail.com>
2023-01-11 20:46:38 +0100

    Add mapped superclass example

---
+++ src/main/java/eu/kaluzinski/sdjpainheritance/domain/mapped_super_class/OrderHeader.java
@@ -0,0 +1,17 @@
+package eu.kaluzinski.sdjpainheritance.domain.mapped_super_class;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class OrderHeader extends BaseEntity {
+
+    private String customerName;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+}
